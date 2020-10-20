7a81d1d950464b5f01e836ea164537bd8188a721
wyf <72241996+wyf9@users.noreply.github.com>
2020-10-20 21:03:25 +0800

    (wyf)update wyfyx:exit

--- yx.bat
+++ yx.bat
@@ -1,24 +1,34 @@
 @echo off
 color 0a
-Mode con cols=68 lines=25
+Mode con cols=68 lines=21
 :go
 title 运行
 cls
 Echo.
 Echo                         运行                   
 Echo     Windows 将根据您所输入的名称,为您打开相应的程序、文件夹、文档
-Echo     或 Internet 资源。
+Echo                       或 Internet 资源。
 Echo.
 set /p name=打开(O):
+if %name%==exit goto exit
 cls
 Echo.
 Echo                         运行
-Echo     Windows 将根据您所输入的名称,为您打开相应的程序、
-Echo     文件夹、文档或 Internet 资源。
+Echo     Windows 将根据您所输入的名称,为您打开相应的程序、文件夹、文档
+Echo                       或 Internet 资源。
 Echo     打开(O):%name%
 Echo             使用管理权限创建此任务。
 Echo.
 title 运行 - %name%
-timeout /nobreak /t 3
+timeout /nobreak /t 1
 start %name%
-goto go
+exit
+:exit
+cls
+Echo.
+Echo                         运行
+Echo     Windows 将根据您所输入的名称,为您打开相应的程序、文件夹、文档
+Echo                       或 Internet 资源。
+Echo     退出中. . .
+Echo.
+timeout /nobreak /t 1
