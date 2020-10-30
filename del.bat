ade5a737be78a3cb7d7837afac5767eb768ded8d
wyf <72241996+wyf9@users.noreply.github.com>
2020-10-30 19:09:41 +0800

    (wyf)update del.bat

---
+++ del.bat
@@ -0,0 +1,31 @@
+@echo off
+color 0a
+title wyfdelfile
+set wj=%1
+set wj2=%2
+set wj3=%3
+set wj4=%4
+set wj5=%5
+set wj6=%6
+set wj7=%7
+set wj8=%8
+set wj9=%9
+Echo del %wj%
+Echo del %wj2%
+Echo del %wj3%
+Echo del %wj4%
+Echo del %wj5%
+Echo del %wj6%
+Echo del %wj7%
+Echo del %wj8%
+Echo del %wj9%
+del %wj% /f /q
+del %wj2% /f /q
+del %wj3% /f /q
+del %wj4% /f /q
+del %wj5% /f /q
+del %wj6% /f /q
+del %wj7% /f /q
+del %wj8% /f /q
+del %wj9% /f /q
+Echo Del ok
